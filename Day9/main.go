9a48af87367fb2ec13ebf668e24fc499b77b8131
Jon Chen <29684743+jonchen727@users.noreply.github.com>
2021-12-23 11:50:33 -0800

    solution for day 9 part 2

--- Day9/main.go
+++ Day9/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 	"time"
@@ -42,19 +43,27 @@ func main() {
 	start := time.Now() //sets current time to start time
 	lines := (FiletoArray("\n", 1))
 	//ilines := StringtoInt(lines)
+	var LowPoints []LowPoint
+	var sum int
+	sum, LowPoints = Answer1(lines, LowPoints)
+	fmt.Println("Answer 1:", sum)
 
-	
-
-
-	fmt.Println("Answer 1:",Answer1(lines))
+	fmt.Println("Answer 2:", Answer2(LowPoints))
 
 	fmt.Println()
 	duration := time.Since(start) //sets duration to time difference since start
 	fmt.Println("This Script took:", duration, "to complete!")
 }
 
-func Answer1(lines []string) int {
+type LowPoint struct {
+	point      []int
+	valleysize int
+}
+
+func Answer1(lines []string, LowPoints []LowPoint) (int, []LowPoint) {
 	var sum int
+	var counter int
+
 	for l, line := range lines {
 
 		for i, index := range line {
@@ -62,20 +71,26 @@ func Answer1(lines []string) int {
 			var left string
 			var top string
 			var bottom string
-
 			var neighbors float64
 			var lower float64
+			var coords [][]int
+			var lower2 int
 
 			x := string(index)
 			ix, _ := strconv.Atoi(x)
 			if i < len(string(line))-1 {
 				right = string(lines[l][i+1])
-
 				iright, _ := strconv.Atoi(right)
 
 				neighbors += 1
 				if iright > ix {
 					lower += 1
+					if iright < 9 {
+						var coord []int
+						lower2 += 1
+						coord = append(coord, i+1, l, iright)
+						coords = append(coords, coord)
+					}
 				}
 
 			}
@@ -85,6 +100,12 @@ func Answer1(lines []string) int {
 				neighbors += 1
 				if ileft > ix {
 					lower += 1
+					if ileft < 9 {
+						var coord []int
+						lower2 += 1
+						coord = append(coord, i-1, l, ileft)
+						coords = append(coords, coord)
+					}
 				}
 
 			}
@@ -95,6 +116,12 @@ func Answer1(lines []string) int {
 				neighbors += 1
 				if ibottom > ix {
 					lower += 1
+					if ibottom < 9 {
+						var coord []int
+						lower2 += 1
+						coord = append(coord, i, l+1, ibottom)
+						coords = append(coords, coord)
+					}
 				}
 
 			}
@@ -105,17 +132,175 @@ func Answer1(lines []string) int {
 				neighbors += 1
 				if itop > ix {
 					lower += 1
+					if itop < 9 {
+						var coord []int
+						lower2 += 1
+						coord = append(coord, i, l-1, itop)
+						coords = append(coords, coord)
+					}
 				}
 
 			}
 
 			if neighbors == lower {
+				var xy []int
+				xy = append(xy, i, l)
+				lowpoint := LowPoint{point: xy}
+				LowPoints = append(LowPoints, lowpoint)
+
+				ValleyInspector(coords, lines, LowPoints, counter)
+
 				sum += (ix + 1)
+				counter += 1
+
 			}
 		}
 
 	}
-
-	return sum
+	return sum, LowPoints
 }
 
+func ValleyInspector(coords [][]int, lines []string, LowPoints []LowPoint, counter int) {
+	var iteration int
+
+	coordsfinal := coords
+
+	for {
+
+		//fmt.Println("Iteration:", iteration)
+		//fmt.Println("Input:", coords)
+
+		coordsprev := LowerFinder(coords, lines, LowPoints, counter)
+		//fmt.Println(coordsprev)
+		//fmt.Println("Len CoordsPrev:",len(coordsprev))
+		coords = removeDuplicateNestedInt(coordsprev)
+		//fmt.Println("Len Coords:",len(coords))
+		//fmt.Println("Output:", coords)
+		for _, coord := range coords {
+			coordsfinal = append(coordsfinal, coord)
+		}
+		coordsfinal = removeDuplicateNestedInt(coordsfinal)
+
+		iteration += 1
+		if len(coords) == 0 {
+			//fmt.Println("Final:", coordsfinal, len(coordsfinal))
+			LowPoints[counter].valleysize = len(coordsfinal) + 1
+			break
+		}
+
+	}
+}
+
+func LowerFinder(coords [][]int, lines []string, LowPoints []LowPoint, counter int) [][]int {
+	var coords2 [][]int
+	for _, coord := range coords {
+		i := coord[0]
+		l := coord[1]
+		ix := coord[2]
+		var right string
+		var left string
+		var top string
+		var bottom string
+		var neighbors float64
+		var lower float64
+		line := lines[0]
+
+		lower = 0
+
+		if i < len(string(line))-1 {
+			right = string(lines[l][i+1])
+			iright, _ := strconv.Atoi(right)
+
+			if iright != 9 && iright > ix {
+				lower += 1
+				var coord2 []int
+				coord2 = append(coord2, i+1, l, iright)
+				coords2 = append(coords2, coord2)
+			}
+
+		}
+		if i > 0 {
+			left = string(lines[l][i-1])
+			ileft, _ := strconv.Atoi(left)
+			neighbors += 1
+			if ileft != 9 && ileft > ix {
+				lower += 1
+				var coord2 []int
+				coord2 = append(coord2, i-1, l, ileft)
+				coords2 = append(coords2, coord2)
+			}
+
+		}
+
+		if l < len(lines)-1 {
+			bottom = string(lines[l+1][i])
+			ibottom, _ := strconv.Atoi(bottom)
+			neighbors += 1
+			if ibottom != 9 && ibottom > ix {
+				lower += 1
+				var coord2 []int
+				coord2 = append(coord2, i, l+1, ibottom)
+				coords2 = append(coords2, coord2)
+			}
+
+		}
+
+		if l > 0 {
+			top = string(lines[l-1][i])
+			itop, _ := strconv.Atoi(top)
+			neighbors += 1
+			if itop != 9 && itop > ix {
+				lower += 1
+				var coord2 []int
+				coord2 = append(coord2, i, l-1, itop)
+				coords2 = append(coords2, coord2)
+			}
+
+		}
+
+	}
+	return coords2
+}
+
+func removeDuplicateNestedInt(coords [][]int) [][]int {
+	var strSlice []string
+	newcords := [][]int{}
+	for _, coord := range coords {
+		var newstr string
+		for _, nums := range coord {
+			snums := strconv.Itoa(nums)
+			newstr += snums
+
+		}
+		strSlice = append(strSlice, newstr)
+	}
+
+	allKeys := make(map[string]bool)
+	list := []string{}
+	for i, item := range strSlice {
+		if _, value := allKeys[item]; !value {
+			allKeys[item] = true
+			list = append(list, item)
+			newcords = append(newcords, coords[i])
+
+			//fmt.Println(i,value,list)
+		} else {
+			//fmt.Println(i,value,list)
+		}
+
+	}
+	//fmt.Println(newcords)
+	return newcords
+}
+
+func Answer2(LowPoints []LowPoint) int {
+	product := 1
+	sort.Slice(LowPoints[:], func(i, j int) bool {
+		return LowPoints[i].valleysize > LowPoints[j].valleysize
+	})
+
+	for i := 0; i < 3; i++ {
+		product *= LowPoints[i].valleysize
+	}
+	return product
+}
